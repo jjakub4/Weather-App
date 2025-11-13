666c3016d840f33f95321e7fb957c26cea65ebb4
Jakub <jsak2570@gmail.com>
2025-11-13 16:43:58 +0100

    hourly forecast dropdown added

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const unitBtn = document.getElementById("unit-dropdown-btn");
 const unitDropdown = document.getElementById("unit-dropdown-menu");
 
+const hourlyBtn = document.getElementById("dropdown-hourly-btn");
+const hourlyDropdown = document.getElementById("dropdown-hourly-menu");
+
 unitBtn.addEventListener("click", () => {
   unitDropdown.style.display =
     unitDropdown.style.display === "flex" ? "none" : "flex";
 });
 
+hourlyBtn.addEventListener("click", () => {
+  hourlyDropdown.style.display =
+    hourlyDropdown.style.display === "flex" ? "none" : "flex";
+});
+
 document.addEventListener("click", (event) => {
-  if (!unitBtn.contains(event.target)) {
+  if (!unitBtn.contains(event.target) || !hourlyBtn.contains(event.target)) {
     unitDropdown.style.display = "none";
   }
 });
